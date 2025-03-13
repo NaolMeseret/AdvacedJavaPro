b5a8016cfbf4a0cd10b529c60df4b4304f9b3cdf
Naol <naolmes15062015@gmail.com>
2025-03-13 09:38:24 +0300

    march13

---
+++ JavaFXApplication/src/javafxapplication/First.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class JavaFXApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        TextField textField = new TextField();
+        textField.setPromptText("Enter text");
+        
+        Button resetButton = new Button("Reset");
+        Button saveButton = new Button("Save");
+        
+        resetButton.setOnAction(e -> textField.clear());
+        saveButton.setOnAction(e -> System.out.println("Saved: " + textField.getText()));
+        
+        HBox hbox = new HBox(10, textField, resetButton, saveButton);
+        Scene scene = new Scene(hbox, 300, 50);
+        
+        primaryStage.setTitle("TextField Example");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
