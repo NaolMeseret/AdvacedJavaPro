d3ae6f4567ae5f6b9fe881517bb0710b99b4e0de
Naol <naolmes15062015@gmail.com>
2025-03-19 14:45:23 +0300

    arch19

--- BroCode/src/brocode/BroCode.java
+++ BroCode/src/brocode/BroCode.java
@@ -1,15 +1,13 @@
-//package brocode;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Polygon;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class BroCode extends Application {
@@ -19,52 +17,24 @@ public class BroCode extends Application {
     }
 
     @Override
-    public void start(Stage stage) {
-        Group root = new Group();
-        Scene scene = new Scene(root);
+    public void start(Stage stage) throws Exception {
+        Button btn = new Button();
+        Button exit = new Button();
 
-        Text text = new Text();
-        text.setText("Woow!");
-        text.setX(50);
-        text.setY(50);
-        text.setFont(Font.font(50));
-        text.setFill(Color.BLUE);
+        btn.setText("Click me");
+        exit.setText(" Exit");
 
-        Line line = new Line();
-        line.setStartX(200);
-        line.setStartY(200);
-        line.setEndX(500);
-        line.setEndY(200);
-        line.setStrokeWidth(5);
-        line.setStroke(Color.RED);
-        line.setOpacity(.5);
-        line.setRotate(45);
+     
+        exit.setOnAction(e -> {
+            System.out.println("Bye");
+            System.exit(0);
+        });
 
-        Rectangle rectangle = new Rectangle();
-        rectangle.setX(100);
-        rectangle.setY(100);
-        rectangle.setWidth(100);
-        rectangle.setHeight(100);
-        rectangle.setFill(Color.BLUE);
-        rectangle.setStrokeWidth(5);
-        rectangle.setStroke(Color.BLACK);
-
-        Polygon trangle = new Polygon();
-        trangle.getPoints().addAll(200.0, 200.0, 300.0 , 300.0, 200.0 , 300.0);
-        trangle.setFill(Color.YELLOW);
-        
-        Circle circle = new Circle();
-        circle.setCenterX(350);
-        circle.setCenterY(350);
-        circle.setRadius(50);
-        circle.setFill(Color.ORANGE);
-        
-        root.getChildren().add(text);
-        root.getChildren().add(line);
-        root.getChildren().add(rectangle);
-        root.getChildren().add(trangle);
-        root.getChildren().add(circle);
+        VBox root = new VBox();
+        root.getChildren().addAll(btn, exit);
+        Scene scene = new Scene(root, 500, 300);
         stage.setScene(scene);
+
         stage.show();
     }
 
